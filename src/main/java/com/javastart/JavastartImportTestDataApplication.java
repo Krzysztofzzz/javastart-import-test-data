9d9e7143021e9eb040d07b8b6ace4f153e19328e
Krzysztof <krzubrycki@gmail.com>
2022-02-10 15:39:35 +0100

    getting data through repository

--- src/main/java/com/javastart/JavastartImportTestDataApplication.java
+++ src/main/java/com/javastart/JavastartImportTestDataApplication.java
@@ -1,13 +1,17 @@
 package com.javastart;
 
+import com.javastart.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JavastartImportTestDataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavastartImportTestDataApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(JavastartImportTestDataApplication.class, args);
+        UserRepository userRepository = context.getBean(UserRepository.class);
+        userRepository.findAll().forEach(System.out::println);
     }
 
 }
